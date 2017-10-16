956a78f2186ecb23b37e71ec5afef2b7b3f544da
Artyom <artyom.zayarny@gmail.com>
2017-10-16 23:17:06 +0300

    modal

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.btn').click(function() {
+	$('.modal').show();
+	$('.wrapper').css({visibility: "visible"});
+});
+$('.close').click(function() {
+	$('.modal').hide();
+	$('.wrapper').css({visibility: "hidden"});
+});
+	
+	
+	
+	
+
+
+
+
+
+
+
